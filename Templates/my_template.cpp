8ffc63f896a229600d685824b1a2131fb963ce65
Mohamed Karam <111219555+mohammed-karam@users.noreply.github.com>
2025-08-02 11:18:01 +0300

    Create my_template.cpp

---
+++ Templates/my_template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+
+void solve() {
+}
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
+
