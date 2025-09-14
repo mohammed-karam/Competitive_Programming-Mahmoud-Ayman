304d8cfe9c9075dbaf7ae6221f0dc8c411cc09e0
Mohamed Karam <111219555+mohammed-karam@users.noreply.github.com>
2025-09-14 12:04:03 +0300

    Update print path on a tree.cpp

--- Templates/print path on a tree.cpp
+++ Templates/print path on a tree.cpp
@@ -1,16 +1,16 @@
-bool dfs_path(int v, int p, int target, vector<vector<int>> &g, vector<int> &path) {
+bool found;
+vector<int> ans, path;
+ 
+void dfs(int v, int p, vector<vector<int>> &g, int f){
     path.push_back(v);
-    if (v == target) return true;
-    for (int u : g[v]) {
-        if (u == p) continue;
-        if (dfs_path(u, v, target, g, path)) return true;
+    if(v == f){
+        ans = path;
+        found = true;
+        return;
+    }
+    for(int u: g[v]){
+        if(u != p) dfs(u, v, g, f);
+        if (found) return;
     }
     path.pop_back();
-    return false;
-}
-
-vector<int> get_path(int x, int y, vector<vector<int>> &g) {
-    vector<int> path;
-    dfs_path(x, -1, y, g, path);
-    return path;
 }
