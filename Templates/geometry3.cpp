b1f9379e2ace6a24ee98378bf9a330c2cbb0f84f
Mohamed Karam Manaa El saadany <111219555+mohammed-karam@users.noreply.github.com>
2026-06-16 09:06:50 +0300

    Add geometry functions for convex hull and half-planes

---
+++ Templates/geometry3.cpp
@@ -0,0 +1,242 @@
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = sgn(orient(a, b, c));
+    return o < 0 || (include_collinear && o == 0);
+}
+
+bool collinear(pt a, pt b, pt c) { return sgn(orient(a, b, c)) == 0; }
+
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = sgn(orient(p0, a, b));
+        if (o == 0)
+            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
+                   < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+        return o < 0;
+    });
+    if (include_collinear) {
+        int i = (int)a.size()-1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin()+i+1, a.end());
+    }
+
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        if(st.empty() || a[i] != st.back())
+            st.push_back(a[i]);
+    }
+
+    if (include_collinear == false && st.size() == 2 && st[0] == st[1])
+        st.pop_back();
+
+    a = st;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+void reorder_polygon(vector<pt> & P){
+    size_t pos = 0;
+    for(size_t i = 1; i < P.size(); i++){
+        if(P[i].y < P[pos].y || (P[i].y == P[pos].y && P[i].x < P[pos].x))
+            pos = i;
+    }
+    rotate(P.begin(), P.begin() + pos, P.end());
+}
+
+//p must be counter clockwise
+
+vector<pt> minkowski(vector<pt> P, vector<pt> Q){
+    // the first vertex must be the lowest
+    reorder_polygon(P);
+    reorder_polygon(Q);
+    // we must ensure cyclic indexing
+    P.push_back(P[0]);
+    P.push_back(P[1]);
+    Q.push_back(Q[0]);
+    Q.push_back(Q[1]);
+    // main part
+    vector<pt> result;
+    size_t i = 0, j = 0;
+    while(i < P.size() - 2 || j < Q.size() - 2){
+        result.push_back(P[i] + Q[j]);
+        auto cross = (P[i + 1] - P[i]).cross(Q[j + 1] - Q[j]);
+        if(cross >= 0 && i < P.size() - 2)
+            ++i;
+        if(cross <= 0 && j < Q.size() - 2)
+            ++j;
+    }
+    return result;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Basic half-plane struct.
+struct Halfplane {
+
+    // 'p' is a passing point of the line and 'pq' is the direction vector of the line.
+    pt p, pq;
+    long double angle;
+
+    Halfplane() {}
+    Halfplane(const pt& a, const pt& b) : p(a), pq(b - a) {
+        angle = atan2l(pq.y, pq.x);
+    }
+
+    // Check if point 'r' is outside this half-plane.
+    // Every half-plane allows the region to the LEFT of its line.
+    bool out(const pt& r) {
+        return cross(pq, r - p) < -EPS;
+    }
+
+    // Comparator for sorting.
+    bool operator < (const Halfplane& e) const {
+        return angle < e.angle;
+    }
+
+    // Intersection point of the lines of two half-planes. It is assumed they're never parallel.
+    friend pt inter(const Halfplane& s, const Halfplane& t) {
+        long double alpha = cross((t.p - s.p), t.pq) / cross(s.pq, t.pq);
+        return s.p + (s.pq * alpha);
+    }
+};
+
+
+vector<pt> hp_intersect(vector<Halfplane>& H) {
+    const int inf = 1e9;
+    pt box[4] = {  // Bounding box in CCW order
+            pt(inf, inf),
+            pt(-inf, inf),
+            pt(-inf, -inf),
+            pt(inf, -inf)
+    };
+
+    for(int i = 0; i<4; i++) { // Add bounding box half-planes.
+        Halfplane aux(box[i], box[(i+1) % 4]);
+        H.push_back(aux);
+    }
+
+    // Sort by angle and start algorithm
+    sort(H.begin(), H.end());
+    deque<Halfplane> dq;
+    int len = 0;
+    for(int i = 0; i < int(H.size()); i++) {
+
+        // Remove from the back of the deque while last half-plane is redundant
+        while (len > 1 && H[i].out(inter(dq[len-1], dq[len-2]))) {
+            dq.pop_back();
+            --len;
+        }
+
+        // Remove from the front of the deque while first half-plane is redundant
+        while (len > 1 && H[i].out(inter(dq[0], dq[1]))) {
+            dq.pop_front();
+            --len;
+        }
+
+        // Special case check: Parallel half-planes
+        if (len > 0 && fabsl(cross(H[i].pq, dq[len-1].pq)) < EPS) {
+            // Opposite parallel half-planes that ended up checked against each other.
+            if (dot(H[i].pq, dq[len-1].pq) < 0.0)
+                return vector<pt>();
+
+            // Same direction half-plane: keep only the leftmost half-plane.
+            if (H[i].out(dq[len-1].p)) {
+                dq.pop_back();
+                --len;
+            }
+            else continue;
+        }
+
+        // Add new half-plane
+        dq.push_back(H[i]);
+        ++len;
+    }
+
+    // Final cleanup: Check half-planes at the front against the back and vice-versa
+    while (len > 2 && dq[0].out(inter(dq[len-1], dq[len-2]))) {
+        dq.pop_back();
+        --len;
+    }
+
+    while (len > 2 && dq[len-1].out(inter(dq[0], dq[1]))) {
+        dq.pop_front();
+        --len;
+    }
+
+    // Report empty intersection if necessary
+    if (len < 3) return vector<pt>();
+
+    // Reconstruct the convex polygon from the remaining half-planes.
+    vector<pt> ret(len);
+    for(int i = 0; i+1 < len; i++) {
+        ret[i] = inter(dq[i], dq[i+1]);
+    }
+    ret.back() = inter(dq[len-1], dq[0]);
+    return ret;
+}
+////////////////////////////////////////////////////////////////////////////////////////
+
+vector<pair<int, int>> all_anti_podal(int n, vector<pt> &p) {
+    vector<pair<int, int>> result;
+
+    auto nx = [&](int i){return (i+1)%n;};
+    auto pv = [&](int i){return (i-1+n)%n;};
+
+    // parallel edges should't be visited twice
+    vector<bool> vis(n, false);
+
+    for (int p1 = 0, p2 = 0; p1 < n; ++p1) {
+        // the edge that we are going to consider in this iteration
+        // the datatype is Point, but it acts as a vector
+        pt base = p[nx(p1)] - p[p1];
+
+        // the last condition makes sure that the cross products don't have the same sign
+        while (p2 == p1 || p2 == nx(p1) || sgn(cross(base, p[nx(p2)] - p[p2])) == sgn(cross(base, p[p2] - p[pv(p2)]))) {
+            p2 = nx(p2);
+        }
+
+        if (vis[p1]) continue;
+        vis[p1] = true;
+
+        result.push_back({p1, p2});
+        result.push_back({nx(p1), p2});
+
+        // if both edges from p1 and p2 are parallel to each other
+        if (sgn(cross(base, p[nx(p2)] - p[p2])) == 0) {
+            result.push_back({p1, nx(p2)});
+            result.push_back({nx(p1), nx(p2)});
+            vis[p2] = true;
+        }
+    }
+
+    return result;
+}
+
+
+
+
+// maximum distance from a convex polygon to another convex polygon
+double maximum_dist_from_polygon_to_polygon(vector<PT> &u, vector<PT> &v){ //O(n)
+    int n = (int)u.size(), m = (int)v.size();
+    double ans = 0;
+    if (n < 3 || m < 3) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) ans = max(ans, dist2(u[i], v[j]));
+        }
+        return sqrt(ans);
+    }
+    if (u[0].x > v[0].x) swap(n, m), swap(u, v);
+    int i = 0, j = 0, step = n + m + 10;
+    while (j + 1 < m && v[j].x < v[j + 1].x) j++ ;
+    while (step--) {
+        if (cross(u[(i + 1)%n] - u[i], v[(j + 1)%m] - v[j]) >= 0) j = (j + 1) % m;
+        else i = (i + 1) % n;
+        ans = max(ans, dist2(u[i], v[j]));
+    }
+    return sqrt(ans);
+}
